5ebaaeb5e74694d3f3c49cc51969ef9d87d4f7ea
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-06-06 16:41:17 -0700

    fix services.xserver deprecation warning

--- configuration.nix
+++ configuration.nix
@@ -49,8 +49,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Enable OpenGL and add video drivers
   hardware.graphics = {

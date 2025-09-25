f8e0037dc7523e3a8baf348877c72785ba6bffe8
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-09-25 14:03:44 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,7 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [ rocmPackages.clr.icd ];
   };
   services.xserver.videoDrivers = [ "amdgpu" ];
 

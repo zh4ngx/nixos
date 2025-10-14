f5045e0095b71d52928da56fe6ce0c8565ea995b
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-10-14 14:32:52 -0700

    Add nh

--- home.nix
+++ home.nix
@@ -75,6 +75,12 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/andy/nixos-config"; # sets NH_OS_FLAKE variable for you
+  };
   # --- Hyprland configuration ---
   wayland.windowManager.hyprland = {
     enable = true;

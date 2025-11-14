0079771bbecb2f18aa35de5e678f46b4d55149f9
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-11-13 16:27:17 -0800

    Add usability arguments to rg

--- home.nix
+++ home.nix
@@ -221,7 +221,15 @@ rec {
   };
 
   programs.bat.enable = true;
-  programs.ripgrep.enable = true;
+  programs.ripgrep = {
+    enable = true;
+    arguments = [
+      "--max-columns=150"
+      "--max-columns-preview"
+      "--colors=line:style:bold"
+      "--smart-case"
+    ];
+  };
   programs.rio = {
     enable = true;
     settings = {

9c47973e13bfe523c49ca5382e985f986965499e
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-11-25 18:24:56 -0800

    Add B550 host

--- flake.nix
+++ flake.nix
@@ -26,6 +26,21 @@
             }
           ];
         };
+        B550 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/B550/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.andy = import ./home.nix;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+          ];
+        };
       };
     };
 }
---
+++ hosts/B550/configuration.nix
@@ -0,0 +1,28 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules/common.nix
+  ];
+
+  networking.hostName = "B550"; # Define your hostname.
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  services.udev.packages = with pkgs; [ apio-udev-rules ];
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="pci", DRIVER=="pcieport", ATTR{power/wakeup}="disabled"
+  '';
+
+  hardware.openrazer = {
+    enable = true;
+    users = [ "andy" ];
+  };
+}

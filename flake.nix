e9c0f1cdc2b57eeab7bd8689a3cc7bc3bdfd1dbf
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-12-08 17:37:27 -0800

    iso flake with module

--- flake.nix
+++ flake.nix
@@ -21,6 +21,23 @@
           specialArgs = attrs;
           modules = [ ./hosts/B550/configuration.nix ];
         };
+        iso = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = attrs;
+          modules = [
+            (
+              { modulesPath, ... }:
+              {
+                imports = [
+                  (modulesPath + "/installer/cd-dvd/installation-cd-graphical-base.nix")
+                  ./modules/nixos
+                  home-manager.nixosModules.home-manager
+                  ./modules/home-manager
+                ];
+              }
+            )
+          ];
+        };
       };
     };
 }

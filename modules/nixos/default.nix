91620c2e33dda66cc8cbc1ff22fe3650ba17f1c3
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2025-12-29 15:26:08 -0800

    Limit to 10 configs in bootloader

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -6,7 +6,11 @@
 
 {
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 10;
+  };
+
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Latest kernel

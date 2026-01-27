3030a3d5739798c12a64c96f83cdcba36828c62c
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2026-01-27 15:41:56 -0800

    clean up ollama config, use vulkan

--- modules/nixos/hardware/amd-6900xt.nix
+++ modules/nixos/hardware/amd-6900xt.nix
@@ -1,12 +1,9 @@
 { pkgs, ... }:
 
 {
-  nixpkgs.config.rocmSupport = true;
   services.ollama = {
     enable = true;
-    package = pkgs.ollama-rocm;
-    rocmOverrideGfx = "10.3.0";
+    package = pkgs.ollama-vulkan;
+    loadModels = [ "qwen2.5-coder:32b" ];
   };
-
-  services.ollama.loadModels = [ "qwen2.5-coder:32b" ];
 }

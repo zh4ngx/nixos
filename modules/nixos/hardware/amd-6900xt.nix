5c1c0610336004ea951abcffb7ba0967380a32e4
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2026-01-27 19:03:09 -0800

    play with searx (not reliable)

--- modules/nixos/hardware/amd-6900xt.nix
+++ modules/nixos/hardware/amd-6900xt.nix
@@ -1,5 +1,7 @@
-{ pkgs, ... }:
-
+{ pkgs, inputs, ... }:
+let
+  stable-pkgs = import inputs.nixpkgs-stable { system = "x86_64-linux"; };
+in
 {
   services.ollama = {
     enable = true;
@@ -20,27 +22,44 @@
     environment = {
       OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
       WEBUI_AUTH = "False";
+      ENABLE_PERSISTENT_CONFIG = "False";
       # Web Search Connectivity
       ENABLE_RAG_WEB_SEARCH = "True";
       RAG_WEB_SEARCH_ENGINE = "searxng";
-      RAG_WEB_SEARCH_BASE_URL = "http://127.0.0.1:8888";
+      RAG_WEB_SEARCH_BASE_URL = "http://127.0.0.1<query>&format=json";
+      # This makes the model "think" before searching (Gemini style)
+      ENABLE_SEARCH_QUERY_GENERATION = "True";
     };
   };
 
   services.searx = {
     enable = true;
+    package = stable-pkgs.searxng;
     redisCreateLocally = false;
     settings = {
       server = {
         port = 8888;
         bind_address = "127.0.0.1"; # This is your real security
         secret_key = "placeholder_key_for_local_use"; # Safe for public GitHub
+        limiter = false; # Disable the internal rate-limiter
+        public_instance = false;
       };
+      user_agent_override = "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0";
       search.formats = [
         "html"
         "json"
       ];
       engines = [
+        {
+          name = "brave";
+          engine = "brave";
+          shortcut = "br";
+        }
+        {
+          name = "qwant";
+          engine = "qwant";
+          shortcut = "qw";
+        }
         {
           name = "duckduckgo";
           engine = "duckduckgo";

e496c0d97628ed429e519897669a7ec08e988f54
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2026-01-27 17:41:46 -0800

    Update zed to use local llm

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -445,6 +445,25 @@
               vscode-json-languageserver.binary.path = "vscode-json-languageserver";
               yaml-language-server.binary.path = "yaml-language-server";
             };
+            language_models = {
+              ollama = {
+                api_url = "http://localhost:11434";
+                low_speed_timeout_in_seconds = 60;
+              };
+            };
+            assistant = {
+              version = "2";
+              default_model = {
+                provider = "ollama";
+                model = "qwen2.5-coder:32b";
+              };
+            };
+            inline_assist = {
+              default_model = {
+                provider = "ollama";
+                model = "qwen2.5-coder:14b";
+              };
+            };
           };
         };
 

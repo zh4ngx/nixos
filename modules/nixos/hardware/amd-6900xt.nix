020ce9b1de6e16fd2e872995349ae49e448dbfed
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2026-01-27 16:29:14 -0800

    add qwen 14b

--- modules/nixos/hardware/amd-6900xt.nix
+++ modules/nixos/hardware/amd-6900xt.nix
@@ -4,7 +4,13 @@
   services.ollama = {
     enable = true;
     package = pkgs.ollama-vulkan;
-    loadModels = [ "qwen2.5-coder:32b" ];
+    loadModels = [
+      "qwen2.5-coder:32b"
+      "qwen2.5-coder:14b"
+    ];
+    environmentVariables = {
+      GGML_VK_VISIBLE_DEVICES = "0";
+    };
   };
   environment.systemPackages = [
     pkgs.amdgpu_top

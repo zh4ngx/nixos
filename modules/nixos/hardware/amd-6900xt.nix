0f8e7b54a64b93875ca06b85b5e0ba8cd399bbec
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2026-01-27 17:38:11 -0800

    add open-webui for ollama

--- modules/nixos/hardware/amd-6900xt.nix
+++ modules/nixos/hardware/amd-6900xt.nix
@@ -12,6 +12,14 @@
       GGML_VK_VISIBLE_DEVICES = "0";
     };
   };
+
+  services.open-webui = {
+    enable = true;
+    environment = {
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+    };
+  };
+
   environment.systemPackages = [
     pkgs.amdgpu_top
   ];

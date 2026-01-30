5580e3f8d59611ee1c1455f455ad8bcc44a8e6ff
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2026-01-30 15:13:50 -0800

    add bitwarden desktop + cli

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -42,6 +42,8 @@
           #   echo "Hello, ${config.home.username}!"
           # '')
           beeper
+          bitwarden-desktop
+          bitwarden-cli
           gnomeExtensions.appindicator
           gnomeExtensions.hide-top-bar
           gnomeExtensions.just-perfection

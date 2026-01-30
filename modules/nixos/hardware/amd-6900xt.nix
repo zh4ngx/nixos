642d0c9aef592eb1e835b8bf600d047636f3e968
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2026-01-30 11:22:04 -0800

    Revert "add local searx"
    This reverts commit 0703a0c733c179b9adae8d9b7842b25dc38ed2a0.

--- modules/nixos/hardware/amd-6900xt.nix
+++ modules/nixos/hardware/amd-6900xt.nix
@@ -20,36 +20,6 @@
     environment = {
       OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
       WEBUI_AUTH = "False";
-      # Web Search Connectivity
-      ENABLE_RAG_WEB_SEARCH = "True";
-      RAG_WEB_SEARCH_ENGINE = "searxng";
-      RAG_WEB_SEARCH_BASE_URL = "http://127.0.0.1:8888";
-    };
-  };
-
-  services.searx = {
-    enable = true;
-    redisCreateLocally = false;
-    settings = {
-      server = {
-        port = 8888;
-        bind_address = "127.0.0.1"; # This is your real security
-        secret_key = "placeholder_key_for_local_use"; # Safe for public GitHub
-      };
-      search.formats = [
-        "html"
-        "json"
-      ];
-      engines = [
-        {
-          name = "duckduckgo";
-          engine = "duckduckgo";
-        }
-        {
-          name = "wikipedia";
-          engine = "wikipedia";
-        }
-      ];
     };
   };
 

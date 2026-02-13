fdecadb76f4d23cea0d6da8e55ea340859510d6e
Andy Zhang <1329212+zh4ngx@users.noreply.github.com>
2026-02-13 02:04:09 -0800

    update subs / uosc

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -370,6 +370,13 @@
             cscale = "krigbilateral";
             # Audio mixing (biased stereo)
             af = "lavfi=[pan=stereo|FL=FC+0.707*FL+0.5*SL+0.5*BL+0.5*LFE|FR=FC+0.707*FR+0.5*SR+0.5*BR+0.5*LFE]";
+            sub-auto = "fuzzy";
+            slang = "eng,en";
+            sub-visibility = "yes";
+            # UOSC
+            osc = "no"; # Disable the default, blocky UI
+            border = "no"; # Let uosc draw its own modern window border
+            osd-bar = "no"; # Let uosc handle the volume/seek bars
           };
           profiles = {
             SDR-Reference = {

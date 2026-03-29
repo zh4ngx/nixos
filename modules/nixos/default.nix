b443c6dbf5f6635e4424c7c573e42062602ca649
Andy Zhang <zh4ng@noreply.codeberg.org>
2026-03-28 17:26:30 -0700

    claude: Remove max token

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -40,8 +40,7 @@
             "CLAUDE_CODE_EXPERIMENTAL_AGENT_TEAMS": "1",
             "DISABLE_TELEMETRY": "1",
             "CLAUDE_CODE_DISABLE_FEEDBACK_SURVEY": "1",
-            "DISABLE_ERROR_REPORTING": "1",
-            "CLAUDE_CODE_MAX_OUTPUT_TOKENS": "50000"
+            "DISABLE_ERROR_REPORTING": "1"
           },
           "statusLine": {
             "type": "command",
